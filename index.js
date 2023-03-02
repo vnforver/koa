d6d918b845f69ca7ba9d7b7558dd3e8404a74570
vnforver <42856202+vnforver@users.noreply.github.com>
2023-03-02 15:42:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-wx.cloud.callContainer({
-  "config": {
-    "env": "prod-9gp0r7oq5892846c"
-  },
-  "path": "/api/count",
-  "header": {
-    "X-WX-SERVICE": "koa-rztb"
-  },
-  "method": "POST",
-  "data": {
-    "action": "inc"
-  }
-})
+// 一个用户发什么消息，就反弹什么消息的消息回复功能
+router.post('/message/post', async ctx => {
+  const { ToUserName, FromUserName, Content, CreateTime } = ctx.request.body;
+
+  ctx.body = {
+    ToUserName: FromUserName,
+    FromUserName: ToUserName,
+    CreateTime: +new Date(),
+    MsgType: 'text',
+    Content: `反弹你发的消息：${Content}`,
+  };
+});
+
+
 
 
 const Koa = require("koa");
